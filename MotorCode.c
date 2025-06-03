00bfb49e9082808a8b104b725df3e55d88cf7940
Zulkan <zulkanzaqzuq@gmail.com>
2025-06-03 21:46:12 +0700

    Finished Button Init

--- MotorCode.c
+++ MotorCode.c
@@ -42,6 +42,14 @@
 #define GPIO_PORTA_DEN_R        (*((volatile uint32_t *)0x4000451C)) //Offset 0x51c
 #define GPIO_PORTA_DR8R					(*((volatile uint32_t *)0x40004508) //Offset 0x508
 	
+#define GPIO_PORTA_IS_R 				(*((volatile uint32_t *)0x40004404)) //Offset 0x404
+#define GPIO_PORTA_IBE_R 				(*((volatile uint32_t *)0x40004408)) //Offset 0x408
+#define GPIO_PORTA_IEV_R 				(*((volatile uint32_t *)0x4000440C)) //Offset 0x40C
+#define GPIO_PORTA_ICR_R 				(*((volatile uint32_t *)0x4000441C)) //Offset 0x41C
+#define GPIO_PORTA_IM_R 				(*((volatile uint32_t *)0x40004410)) //Offset 0x410
+
+#define NVIC_EN0_R  						(*((volatile uint32_t *)0xE000E100)) //Because interrupt vector for port A is interrupt 0 (OFFSET 0X100)
+	
 
 //ALL ABOUT NVIC (ctrl, reload, pri, current )
 #define NVIC_PRI0_R  						(*((volatile uint32_t *)0xE000E400)) //PRI0 because interrupt 0 ( Port A ) ( OFFSET 0X400)
@@ -90,7 +98,7 @@ void DCMotor_Init(void)
 	
 }
 
-void SysTick_Handler (void)
+void SysTick_Handler (void) // Control how the motor/dynamo operates automatically by systick (maintaining constant number that is set, at the first stage or after changed by pressing switch next time)
 {
 	if(GPIO_PORTA_DATA_R & 0x80) //If PA7 High is true
 	{
@@ -102,4 +110,23 @@ void SysTick_Handler (void)
 		GPIO_PORTA_DATA_R &= 0x80; //turn PA7 High
 		NVIC_STRELOAD_R = High - 1; // Load reload value with High Value inputted
 	}
+}
+
+void Button_Init (uint32_t delay) //we are initiating Edge interrupt for the buttons
+{
+	delay = SYSCTL_RCGCGPIO_R;
+	GPIO_PORTA_IS_R &= ~0x30; //1100 1111, PA4 and PA5 Edge sensitive 
+	GPIO_PORTA_IBE_R &= ~0x30; // Not both Edge
+	GPIO_PORTA_IEV_R &= ~0x30; // Falling Edge event for PA4 & PA5
+	GPIO_PORTA_ICR_R = 0x30; //clear interrupt PA4 & PA5
+	GPIO_PORTA_IM_R |= 0x30; //arm PA4 and PA5 
+	
+	NVIC_PRI0_R = (NVIC_PRI0_R & 0xFFFFFF1F) | 0x20; //priority 1 ( bit 7-5 is 001 which is 1 in decimal, but based on hex postition its 0010 0000 so 0x20)
+	NVIC_EN0_R = 0x01; //enable interrupt 0  port A
+	
+}
+
+void GPIOPortA_Handler(void)
+{
+	
 }
